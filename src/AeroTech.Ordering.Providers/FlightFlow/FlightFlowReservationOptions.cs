654d422085631032febe92a3008a22f1a775bfdc
Ali Farhadi <al.farhadi@alibaba.ir>
2026-09-24 16:08:27 +0330

    Stage 2- Fix Interactions

--- src/AeroTech.Ordering.Providers/FlightFlow/FlightFlowReservationOptions.cs
+++
@@ -1,9 +0,0 @@
-namespace AeroTech.Ordering.Providers.FlightFlow
-{
-    public sealed class FlightFlowReservationOptions
-    {
-        public const string SectionName = "FlightFlow";
-
-        public int HoldMinutes { get; set; }
-    }
-}
